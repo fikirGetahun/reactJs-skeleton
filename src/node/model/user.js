5a94232d82407e30141a9ef88f1b8bb8bafd571c
fikirGetahun <fikirgetahun97@gmail.com>
2022-12-26 13:59:25 +0300

    ggg

---
+++ src/node/model/user.js
@@ -0,0 +1,60 @@
+const jwt=require('jsonwebtoken');
+const config= require('config')
+const mongoose= require('mongoose');
+const Joi = require('joi');
+
+// mongoose.set('strictQuery', false);
+// mongoose.connect('mongodb://0.0.0.0:27017/Customer').then(()=>{
+//     console.log('connected to MongoDb ...')
+// }).catch((error)=>{
+//     console.error('Could not connect to MongoDB ...', error);
+// });
+const userSchema=new mongoose.Schema({
+    name:{type:String,
+         required:true,
+        minlength:5,
+        maxlength:50
+    },
+   email:{
+          type:String,
+          required:true,
+          unique:true,
+          minlength:5,
+          maxlength:255},
+   password:{
+          type:String,
+          required:true,
+          minlength:5,
+          maxlength:1024},
+   isAdmin: Boolean
+         
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign(
+      {
+        _id: this._id ,
+        isAdmin: this.isAdmin   
+      },
+      config.get("jwtPrivateKey")
+    );
+    return token;
+  };
+
+
+
+const user=mongoose.model('User',userSchema)
+
+function validateUser(user){
+    const schema = Joi.object({
+        name: Joi.string().required().min(5).max(50),
+        email: Joi.string() .required().min(5).max(255).email(),
+       password:Joi.string().min(5).max(255).required()
+        
+        
+        });
+           return   schema.validate(user);
+}
+
+exports.User=user;
+exports.Validate=validateUser;
---
+++ src/node/routes/customers.js
@@ -0,0 +1,62 @@
+
+//const asyncMiddleware=require('../middleware/async')
+const auth=require('../middleware/auth');
+const admin = require('../middleware/admin');
+const {customer,Validate} = require('../model/customer')
+const express= require('express');
+const async = require('../middleware/async');
+
+const router= express.Router();
+
+router.get('/', async(req,res)=>{
+ 
+        const cust=await customer.find().sort('name');
+        res.send(cust);
+      
+})
+
+router.get('/:id', async(req,res)=>{
+//console.log('get by id',req.params.id);
+    let cust=await customer.findById(req.params.id);
+
+    if(!cust) res.status(404).send('Not found')
+    else res.send(cust)
+
+    
+})
+// router.post('/',[auth,admin], async(req,res)=>{
+    router.post('/',  async(req,res)=>{
+    ///--Validation using Joi
+    const {error}=    Validate(req.body); 
+    if(error) return    res.status(400).send(error.details[0].message)
+    
+    const cust= customer({name:req.body.name, isGold:req.body.isGold, phone:req.body.phone});
+    let result =await cust.save();
+    res.send(result);
+
+})
+router.put('/:id', async(req,res)=>{
+    const {error}=    Validate(req.body);  
+    if(error){
+       return    res.status(400).send(error.details[0].message)
+    }
+let cust =await   customers.findByIdAndUpdate(req.params.id, {
+     name:req.body.name,
+    phone:req.body.phone,
+    isGold:req.body.isGold},{
+        new:true
+    })
+   
+    if(!cust) res.status(404).send('id Not found')
+       res.send(cust);
+
+})
+router.delete('/:id',auth, async(req,res)=>{
+    let cust=await customer.findOneAndRemove( req.params.id);
+    if(!cust) return res.status(404).send('id Not found')
+    res.send(cust);
+
+
+})
+
+module.exports=router;
