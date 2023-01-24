420c36f4eae8fe5367742c86d96ea5d05bef18f8
fikirGetahun <fikirgetahun97@gmail.com>
2023-01-24 17:08:25 +0300

    now foreal its done the category order swap part

--- src/components/adminComponent/orderManage.js
+++ src/components/adminComponent/orderManage.js
@@ -12,19 +12,20 @@ import PutHandler from "../../service/apiHandler/putHandler";
 
  useEffect(()=>{
     getCategoryOrder()
-    orderDisplay()
- },[])
+  },[])
 
  
 
  
 
-const [catOrder, setCatOrder] = useState([])
+const [items, setItems] = useState([])
+
+
 const getCategoryOrder = async()=>{
     let data = new GetHandler()
     let x =data.getCategoryInOrder().then(res=>{
         if(res.statusText == 'OK'){
-            setCatOrder(res.data)
+            setItems(res.data)
         }else{
             alert('db connect error')
         }
@@ -36,7 +37,7 @@ const orderUpdater = async (body, id)=>{
     let data = new PutHandler()
     await data.updateCategoryOrder(body,id).then(res=>{
         if(res.statusText == 'OK'){
-           alert('ok') 
+        //    alert('ok') 
         }else{
             alert('db error')
         }
@@ -45,123 +46,143 @@ const orderUpdater = async (body, id)=>{
 
 const upMoveHandler = (i, objId)=>{
     //swap the previous with the current
-    let prev = catOrder[i-1]
+    let prev = items[i-1]
 
-    let old = [...catOrder]
+    let old = [...items]
         // const toBeEditedCurrent = old.findIndex(x=> x[i])
         // const toBeEditedPrev = old.findIndex(x=>x[i-1])
-        const toBeEditedCurrent = old.find(obj => obj._id === objId)
-        const toBeEditedPrev = old.find(obj => obj._id === prev._id)
+        let toBeEditedCurrent = old.find(obj => obj._id === objId)
+        let toBeEditedPrev = old.find(obj => obj._id === prev._id)
         console.log(toBeEditedCurrent)
  
         let pName = toBeEditedPrev.name
         let cName = toBeEditedCurrent.name
 
-        toBeEditedCurrent.order = toBeEditedCurrent.order-1
-        toBeEditedCurrent.name = pName
+        let pOrder = toBeEditedPrev.order
+        let cOrder = toBeEditedCurrent.order
 
-        toBeEditedPrev.order = toBeEditedPrev.order +1
+        let cId = toBeEditedCurrent._id
+        let pId = toBeEditedPrev._id
+
+        toBeEditedCurrent.name = pName
+        toBeEditedCurrent.order = cOrder
+       
         toBeEditedPrev.name = cName
+        toBeEditedPrev.order = pOrder
+
+        toBeEditedCurrent._id = pId
+        toBeEditedPrev._id = cId
+        
        
         //db change updater
         let toBeUpdatedCurrent = {
-            order:  toBeEditedCurrent.order-1
+            order:  cOrder
         }
         let toBeUpdatedPrev = {
-            order: toBeEditedPrev.order +1
+            order: pOrder
         }
-
+        setItems(old)
         // current order updater
         orderUpdater(toBeUpdatedCurrent, toBeEditedCurrent._id)
 
         // prev order updater
         orderUpdater(toBeUpdatedPrev, toBeEditedPrev._id)
-        setCatOrder(old)
+       
 }
 
 
 
 const downMoveHandler = (i, objId)=>{
     //swap the previous with the current
-    let prev = catOrder[i+1]
+    let prev = items[i+1]
 
-    let old = [...catOrder]
+    let old = [...items]
         // const toBeEditedCurrent = old.findIndex(x=> x[i])
         // const toBeEditedPrev = old.findIndex(x=>x[i-1])
-        const toBeEditedCurrent = old.find(obj => obj._id === objId)
-        const toBeEditedPrev = old.find(obj => obj._id === prev._id)
-        console.log(toBeEditedCurrent)
+       let toBeEditedCurrent = old.find(obj => obj._id === objId)
+       let toBeEditedPrev = old.find(obj => obj._id === prev._id)
+        // console.log(toBeEditedCurrent)
  
         let pName = toBeEditedPrev.name
         let cName = toBeEditedCurrent.name
 
-        toBeEditedCurrent.order = toBeEditedCurrent.order+1
-        toBeEditedCurrent.name = pName
+        let pOrder = toBeEditedPrev.order
+        let cOrder = toBeEditedCurrent.order
 
-        toBeEditedPrev.order = toBeEditedPrev.order -1
+        let cId = toBeEditedCurrent._id
+        let pId = toBeEditedPrev._id
+
+        toBeEditedCurrent.name = pName
+        toBeEditedCurrent.order = cOrder
+       
         toBeEditedPrev.name = cName
+        toBeEditedPrev.order = pOrder
+
+        toBeEditedCurrent._id = pId
+        toBeEditedPrev._id = cId
+        
        
         //db change updater
         let toBeUpdatedCurrent = {
-            order:  toBeEditedCurrent.order+1
+            order:  cOrder
         }
         let toBeUpdatedPrev = {
-            order: toBeEditedPrev.order -1
+            order: pOrder
         }
-
+        setItems(old)
         // current order updater
         orderUpdater(toBeUpdatedCurrent, toBeEditedCurrent._id)
 
         // prev order updater
         orderUpdater(toBeUpdatedPrev, toBeEditedPrev._id)
-        setCatOrder(old)
+        // console.log('prev ', pOrder)
 }
 
 
-const orderDisplay = ()=>{
-    return (
-        catOrder.map((cat, i)=>{
-                        
-            return(
-            <div className="row">
-                <li className="list-group-item col">{cat.name}</li><div className="col" >
-                    {/* // to hide the up arrow if at begginng  */}
-                    {
-                        i != 0 ?  <span className="btn btn-warning" onClick={()=>upMoveHandler(i,cat._id)} >^</span> : <div></div>
-                    }
-                    {
-                        // this means the last row. length of array and i are equal
-                        catOrder.length != (i+1) ? <span className="btn btn-warning" onClick={()=>downMoveHandler(i,cat._id)}>v    </span> : <div></div>
-                    }
-                     </div>
-            </div>
-            )
-          
-      })
-    )
-}
+ 
 
-useEffect(()=>{
+// useEffect(()=>{
    
-    orderDisplay()
-    // alert('changed')
- },catOrder)
+//     orderDisplay()
+//     // alert('changed')
+//  },[items])   
+
+const itemsDisplay = ()=>{
+    return(            <div>
+        <h2>Category Order Manager</h2>
+        <div className=" d-flex justify-content-center align-itmes-center">
+        <div className="card " style={{width: "18rem"}}>
+        <ul className="list-group list-group-flush">
+             {
+items.map((cat, i)=>{
+                
+return(
+<div className="row">
+    <li className="list-group-item col">{cat.name} </li>    {cat.order}<div className="col" >
+
+        {/* // to hide the up arrow if at begginng  */}
+        {
+            i != 0 ?  <span className="btn btn-warning" onClick={()=>upMoveHandler(i,cat._id)} >^</span> : <div></div>
+        }
+        {
+            // this means the last row. length of array and i are equal
+            items.length != (i+1) ? <span className="btn btn-warning" onClick={()=>downMoveHandler(i,cat._id)}>v    </span> : <div></div>
+        }
+         </div>
+</div>
+)
+
+})                    }
+          
+          
+        </ul>
+        </div>
+        </div>
+    </div>)
+}
 
         return(
-            <div>
-                <h2>Category Order Manager</h2>
-                <div className=" d-flex justify-content-center align-itmes-center">
-                <div className="card " style={{width: "18rem"}}>
-                <ul className="list-group list-group-flush">
-                     {
-                        orderDisplay()
-                    }
-                  
-                  
-                </ul>
-                </div>
-                </div>
-            </div>
+            itemsDisplay()
         )
  }
 
