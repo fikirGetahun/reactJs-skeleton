dabedd88629d438992b4af14d3289d116a823811
fikirGetahun <fikirgetahun97@gmail.com>
2024-05-27 11:52:37 +0300

    some css corrections

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -50,84 +50,67 @@ export default function HomePage() {
     }
 
     return(
-        <div   className="container p-0  mainBG "     >
-            <div className='v-stack'>
-       
-                 <div    className="headerBackground"  >
-        <img     className="headerBackground" src={require('../file/img/akko bg.png')} alt={"test"} />
-                 </div>
-
-            <div className="innerBackground scrollHolder"  >
-            <div      className=" scrollHolder   "    >
-            <h2 className="d-flex justify-content-start  "  style={{  marginLeft:"5%",
-}} >Akko Menu</h2>
-
-            <div className="vstack scrollHolder " style={{backgroundColor:'white'}} >
-               
-                <div className="row" style={{ marginLeft:"4.6%", maxWidth:'35%'
-}}>
-                     
-                    <div onClick={backToHome} className="col mainMenu text-nowrap d-flex align-items-center d-flex justify-content-center menu"    >
-                        Main Menu
-                    </div >
-               
-                    
-                    <div className="col"></div>
-
+        <div className="container p-0 mainBG">
+        <div className="v-stack">
+            <div className="headerBackground">
+                <img
+                    className="headerBackground"
+                    src={require('../file/img/akko bg.png')}
+                    alt="test"
+                />
+            </div>
+            <div className="innerBackground scrollHolder">
+                <div className="scrollHolder">
+                    <h2
+                        className="d-flex justify-content-start"
+                        style={{ marginLeft: "5%" }}
+                    >
+                        Akkoo Menu
+                    </h2>
+                    <div className="vstack scrollHolder" style={{ backgroundColor: 'white' }}>
+                        <div className="row" style={{ marginLeft: "4.6%", maxWidth: '35%' }}>
+                            <div
+                                onClick={backToHome}
+                                className="col mainMenu text-nowrap d-flex align-items-center justify-content-center menu"
+                            >
+                                Main Menu
+                            </div>
+                            <div className="col"></div>
+                        </div>
+                        <div className="hstack p-0" style={{ width: "100%", marginBottom: 15, marginLeft: 0 }}>
+                            <TextField
+                                onChange={(e) => setSearchData(e.target.value)}
+                                placeholder="Type in here…"
+                                size="sm"
+                                color="primary"
+                                endDecorator={
+                                    <Box>
+                                        <img
+                                            src={require('../file/img/search.png')}
+                                            style={{ aspectRatio: 1, height: "3vh" }}
+                                        />
+                                    </Box>
+                                }
+                                style={{
+                                    borderRadius: 10,
+                                    width: "100%",
+                                    marginLeft: "5%",
+                                    marginRight: "5%",
+                                    background: "#F3F7FA",
+                                    border: 0,
+                                }}
+                            />
+                           
+                        </div>
+                    </div>
+                    <div className="categoryHome">
+                        <Outlet />
+                    </div>
                 </div>
-                <div className="hstack  P-0" style={{width:"100%", marginBottom:15, marginLeft:0}}>
-            <TextField  
-
-                onChange ={(e)=>setSearchData(e.target.value)}
-              placeholder="Type in here…"
-                size="sm"
-
-                color="primary"
-               
-                endDecorator={
-                    <Box>
-                    <img  src={require('../file/img/search.png')} style={{aspectRatio:1, height:"3vh"}} />
-                    
-                    </Box>
-                }
-              style={{
-                borderRadius: 10,
-              width:"80%",
-              marginLeft:"5%",
-              marginRight:"2%",
-              background:"#F3F7FA",
-              boarder:0,
-                
-              }}
-               />
-              
-               <img  onClick={handleSearch}  className="m-0 p-1 border" src={require('../file/img/search copy.png')} style={{ borderRadius:5, borderColor:"black", height:"5vh"}} />
-
-              {/* <Link to={"search/"+searchData}>
-              <img     className="m-0 p-1 border" src={require('../file/img/search copy.png')} style={{ borderRadius:5, borderColor:"black", height:"5vh"}} />
-
-              </Link> */}
-          
-            
-            </div>
-            </div>
-
-
-            <div className="categoryHome"   >
-              
-              {/* <CategoryLister/> */}
-                 {/* <FoodLister /> */}
-                
-                     
-                    <Outlet />
-                 
-            
-            </div>
-            </div>
-
-            </div> 
             </div>
         </div>
+    </div>
+    
      
         
     );
