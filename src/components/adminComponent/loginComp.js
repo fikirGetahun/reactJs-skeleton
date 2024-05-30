ac05c6e33dc62b7ac1175b8c8415664352c54afe
fikirGetahun <fikirgetahun97@gmail.com>
2024-05-30 10:32:03 +0300

    last fix

--- src/components/adminComponent/loginComp.js
+++ src/components/adminComponent/loginComp.js
@@ -35,8 +35,9 @@ const LoginComp = () =>{
 }
     return(
         <div>
-                        <h3>Login to <strong>Colorlib</strong></h3>
-            <p className="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+                        <h3>Login to <strong>አኮ Menu</strong></h3>
+                        <h4>ከአያቶቼ ምድር</h4>
+            <p className="mb-4">Akkoo Menu Login </p>
            
               <div className="form-group first">
                 <label  >Username</label>
@@ -49,11 +50,8 @@ const LoginComp = () =>{
               </div>
               
               <div className="d-flex mb-5 align-items-center">
-                <label className="control control--checkbox mb-0"><span className="caption">Remember me</span>
-                  <input type="checkbox"  />
-                  <div className="control__indicator"></div>
-                </label>
-                <span className="ml-auto"><a href="#" className="forgot-pass">Forgot Password</a></span> 
+                 
+                 
               </div>
 
               <input    onClick={loginHandler} value="Log In" className="btn btn-block btn-primary"/>
